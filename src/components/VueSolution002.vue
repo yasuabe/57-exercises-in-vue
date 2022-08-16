8db8f9d5a8fc69f5d8773117a9e344276b957c76
yasuabe2613 <yasu@yasuabe.net>
2022-08-16 11:57:41 +0900

    solve 002

---
+++ src/components/VueSolution002.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>2: Counting the Number of Characters</h2>
+    <p>What is the input string? </p>
+    <input v-model="text" placeholder="Homer"/>
+    <span>{{ nameLength() }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'VueSolution002',
+  data() {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    reverse: function() {
+      this.text = this.text.split('').reverse().join('');
+    },
+    nameLength: function() {
+      return this.text.length;
+    }
+  }
+});
+</script>
